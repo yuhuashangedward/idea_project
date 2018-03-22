afb5af89743d85401817529cc60aa33ff81ad383
yuhuashang <yuhuashang.ok@163.com>
2018-03-22 14:31:31 +0800

    ce

--- src/com/alibaba/c2c/utils/ListUtil.java
+++ src/com/alibaba/c2c/utils/ListUtil.java
@@ -1,11 +1,11 @@
 package com.alibaba.c2c.utils;
 
 public class ListUtil {
+
     public static void main(String[] args){
         System.err.println("Hello World !!!!");
         System.err.println("Hello World !!!!");
         System.err.println("Heeloo world!!!!!");
-
     }
 
 
