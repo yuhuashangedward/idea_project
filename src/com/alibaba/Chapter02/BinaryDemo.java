46125f9cc2ff2ae72c8885ea30c81e2fcadc5398
yuhuashang <yuhuashang@aliyun.cfom>
2018-03-24 17:47:38 +0800

    测试2章

--- src/com/alibaba/Chapter02/BinaryDemo.java
+++ src/com/alibaba/Chapter02/BinaryDemo.java
@@ -1,4 +1,16 @@
 package com.alibaba.Chapter02;
 
 public class BinaryDemo {
+
+    public static void main(String[] args){
+        int i1 = 0x2f;
+        System.out.println("i1: " + Integer.toBinaryString(i1));
+
+        int i2 = 0x2F;
+        System.out.println("i2: " + Integer.toBinaryString(i2));
+
+        int i3 = 0177;
+        System.out.println(" i3: " + Integer.toBinaryString(i3));
+    }
+
 }
